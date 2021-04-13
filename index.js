cb3c38013899203d0518f16c1ebf0d04db5eb807
parksangwoo <b01056721617@gmail.com>
2021-04-13 22:22:47 +0900

    GraphQL

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
 const database = require('./database')
 const { ApolloServer, gql } = require('apollo-server')
+
 const typeDefs = gql`
   type Query {
     teams: [Team]
+    team(id: Int): Team
+    equipments: [Equipment]
+    supplies: [Supply]
+  }
+  type Mutation {
+    deleteEquipment(id: String): Equipment
   }
   type Team {
     id: Int
@@ -12,11 +19,47 @@ const typeDefs = gql`
     mascot: String
     cleaning_duty: String
     project: String
+    supplies: [Supply]
+  }
+  type Equipment {
+    id: String
+    used_by: String
+    count: Int
+    new_or_used: String
+  }
+  type Supply {
+    id: String
+    team: Int
   }
 `
 const resolvers = {
   Query: {
     teams: () => database.teams
+      .map((team) => {
+        team.supplies = database.supplies
+        .filter((supply) => {
+          return supply.team === team.id
+        })
+        return team
+      }),
+    team: (parent, args, context, info) => database.teams
+      .filter((team) => {
+        return team.id === args.id
+      })[0],
+    equipments: () => database.equipments,
+    supplies: () => database.supplies 
+  },
+  Mutation: {
+    deleteEquipment: (parent, args, context, info) => {
+      const deleted = database.equipments
+        .filter((equipment) => {
+          return equipment.id === args.id
+        })[0]
+      database.equipments = database.equipments
+        .filter((equipment) => {
+          return equipment.id !== args.id
+        })
+    }
   }
 }
 const server = new ApolloServer({ typeDefs, resolvers })
